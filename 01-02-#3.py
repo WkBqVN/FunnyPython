e334b6fa29a69381b7cdb8f915397cae58a312d4
WkBqVN <vvk.19.11.1994@gmail.com>
2020-06-04 22:55:58 +0700

    vo-van-khoa

--- 01-02-#3.py
+++ 01-02-#3.py
@@ -1,7 +1,6 @@
 inputNumber = int(input("Please enter the number:"))
 if inputNumber < 3:
-    print("Not Correct the number must > 3")
-    inputNumber = int(input("Please enter again: "))
+    print("0")
 res = 0
 for i in range(3,inputNumber,3):
     res += i
