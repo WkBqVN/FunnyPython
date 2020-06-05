6b6d15d7425d28b05a2feb8de82fb72b79555ef6
WkBqVN <vvk.19.11.1994@gmail.com>
2020-06-05 08:42:15 +0700

    vo-van-khoa

--- 01-02-#1.py
+++ 01-02-#1.py
@@ -1,4 +1,8 @@
 firstNumber = int(input("Please enter the first number: "))
 secondNumber = int(input("Please enther the second numer: "))
+if firstNumber > secondNumber:
+	temp = firstNumber
+	firstNumber = secondNumber
+	secondNumber = temp
 for i in range(firstNumber+1,secondNumber):
     print(i,end=" ")
